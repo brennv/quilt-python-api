a022cb52d019a0e803bcdde4928fe5b0399e3a84
Aneesh Karve <aneesh.karve@gmail.com>
2016-01-30 10:22:52 -0800

    v1

---
+++ curl-all.py
@@ -0,0 +1,27 @@
+#!/path/to/python
+
+import re
+from subprocess import call
+import sys
+
+
+def main(argv):
+  if len(argv) != 4:
+    sys.stderr.write("Usage: %s <list of files> <root url> <dir>\n" % argv[0])
+    return 1
+
+  fname = argv[1]
+  url = argv[2]
+  my_dir = './' + argv[3] + '/'
+  f = open(fname, 'r')
+  raw = f.read()
+  
+  tokens = raw.split()
+  for t in tokens:
+    call(['curl', '-o', my_dir + t, url + t])
+
+  return 1
+
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv))
