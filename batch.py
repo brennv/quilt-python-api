063f1fab1152972cc61e0c00edd5c035d2e658ee
Aneesh Karve <aneesh.karve@gmail.com>
2016-02-17 13:07:39 -0800

    nicer upload names; tweak waiting period

--- batch.py
+++ batch.py
@@ -9,10 +9,10 @@ import time
 import dataset
 
 ENDPOINT = 'https://quilt-heroku.herokuapp.com/'
-CORE_TAGS = ['Human', 'ENCODE', 'ChIP_seq', 'hg19']
+CORE_TAGS = ['Human', 'ENCODE', 'ChIPseq', 'hg19', 'Histone', 'BroadPeak']
 CORE_DESC = 'Sources\nhttps://www.encodeproject.org/\nhttp://hgdownload.cse.ucsc.edu/goldenPath/hg19/encodeDCC/wgEncodeBroadHistone/'
 PUBLIC = True
-WAIT = 60 
+WAIT = 80 
 
 def process(argv):
   if len(argv) != 2:
@@ -30,12 +30,18 @@ def process(argv):
     #pull out tags already covered by core tags
     pretags.remove('Encode')
     pretags.remove('Broad')
+    pretags.remove('Histone')
+    pretags.remove('Pk')
+    #nice name
+    name = 'ENCODE ChIP-seq: ' + ' '.join(pretags)
     tags = map(lambda x: '#' + x, CORE_TAGS + pretags)
     description = ' '.join(tags) + '\n' + CORE_DESC
-    args = "-u akarve -n '%s' -d '%s' -f downloads/%s -p True -x '%s'" % (' '.join(pretags), description, l, passwd)
+    args = "-u akarve -n '%s' -d '%s' -f downloads/%s -p True -x '%s'" % (name, description, l, passwd)
     argv = shlex.split(args)
     #create data set on Quilt 
+    print 'starting %s' % (l)
     dataset.create(argv)
+    #don't hammer
     time.sleep(WAIT)
 
   return 1
