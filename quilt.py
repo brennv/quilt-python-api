0e9de45334fac9e17fe60189754fea8a8e7c55f1
Kevin Moore <kevinemoore@gmail.com>
2016-06-20 17:17:21 -0700

    URL param & get item
    Specifying the Quilt URL lets us test this API on dev and staging Quilt
    servers.
    
    The __getitem__ method allows access to a table’s data with the ‘[]’
    operator.

--- quilt.py
+++ quilt.py
@@ -12,9 +12,9 @@ class Table(object):
     _schema = {}
     _buffer = []
     
-    def __init__(self, auth, id, name, sqlname, description, owner, is_public):
+    def __init__(self, con, id, name, sqlname, description, owner, is_public):
         self.nextlink = None
-        self.auth = auth
+        self.connection = con
         self.id = id
         self.name = name
         self.sqlname = sqlname
@@ -22,8 +22,8 @@ class Table(object):
         self.owner = owner
         self.is_public = is_public
 
-    def __init__(self, auth, data):
-        self.auth = auth
+    def __init__(self, con, data):
+        self.connection = con
         self.id = data.get('id')
         self.name = data.get('name')
         self.sqlname = data.get('sqlname')
@@ -37,18 +37,24 @@ class Table(object):
     @property
     def columns(self):
         if not self._schema:
-            response = requests.get("%s/tables/%s/" % (QUILT_URL, self.id),
+            response = requests.get("%s/tables/%s/" % (self.connection.url, self.id),
                                     headers=HEADERS,
-                                    auth=self.auth)
+                                    auth=self.connection.auth)
             data = response.json()
             if data.has_key('columns'):
                 self._schema = data.get('columns')
         return self._schema
 
+    def __getitem__(self, qrid):
+        response = requests.get("%s/data/%s/rows/%s" % (self.connection.url, self.id, qrid),
+                                headers=HEADERS,
+                                auth=self.connection.auth)
+        return response.json()
+
     def __iter__(self):
         self._buffer = []
         self._generator = rowgen(self._buffer)
-        self.nextlink = "%s/data/%s/rows/" % (QUILT_URL, self.id)
+        self.nextlink = "%s/data/%s/rows/" % (self.connection.url, self.id)
         return self
 
     def next(self):        
@@ -59,7 +65,7 @@ class Table(object):
             if self.nextlink:
                 response = requests.get(self.nextlink,
                                         headers=HEADERS,
-                                        auth=self.auth)
+                                        auth=self.connection.auth)
                 data = response.json()
                 self.nextlink = data['next']
                 print self.nextlink
@@ -75,12 +81,13 @@ class Table(object):
 # Use getpass to enter 
 class Connection(object):
     
-    def __init__(self, username):
+    def __init__(self, username, url=QUILT_URL):
+        self.url = url
         self.username = username
         self.password = getpass.getpass()
         self.auth = requests.auth.HTTPBasicAuth(self.username, self.password)
 
-        #response = requests.get("%s/users/%s/" % (QUILT_URL, username),
+        #response = requests.get("%s/users/%s/" % (self.url, username),
         #                       headers=HEADERS,
         #                       auth=requests.auth.HTTPBasicAuth(self.username, self.password))        
         
@@ -88,12 +95,12 @@ class Connection(object):
         #print response.json()        
 
     def get_table(self, table_id):
-        response = requests.get("%s/tables/%s/" % (QUILT_URL, table_id),
+        response = requests.get("%s/tables/%s/" % (self.url, table_id),
                                 headers=HEADERS,
                                 auth=self.auth)        
         
         print response.status_code
-        return Table(self.auth, response.json())
+        return Table(self, response.json())
 
         
         
