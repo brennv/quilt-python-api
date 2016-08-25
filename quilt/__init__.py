733342aec72fdaeb4c773156cd6a85e408a81ac7
Kevin Moore <kevinemoore@gmail.com>
2016-08-25 14:32:06 -0700

    Cleanup extra import

--- quilt/__init__.py
+++ quilt/__init__.py
@@ -2,17 +2,3 @@
 from .util import File, Quilt
 from .table import Table, Column
 from .connection import Connection
-
-try:
-    import pandas
-    PANDAS = True
-except:
-    PANDAS = False
-
-try:
-    import psycopg2
-    import sqlalchemy
-    SQLALCHEMY = True
-except:
-    SQLALCHEMY = False
-
