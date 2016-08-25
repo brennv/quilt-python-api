5d9baba110b8f7d07d2298fe0682d2f323090b5f
Kevin Moore <kevinemoore@gmail.com>
2016-08-25 14:35:56 -0700

    Updating release tag in setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def readme():
 setup(
     name='quilt',
     packages=['quilt'],
-    version='0.1.0',
+    version='0.1.2',
     description='Quilt Python API https://quiltdata.com',
     long_description=readme(),
     classifiers=[
