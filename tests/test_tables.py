17de2563f80715ac79215a57b0e9def818ebe34e
brennv <brennv@users.noreply.github.com>
2016-09-16 22:55:02 -0700

    add save_df tests

--- tests/test_tables.py
+++ tests/test_tables.py
@@ -1,44 +1,40 @@
 import os
-import pytest
-from quilt import Connection
 from random import randint
+from quilt import Connection
+import pandas as pd
 
 
 username = os.environ.get('QUILT_USERNAME')
 url = os.environ.get('QUILT_URL')
+conn = Connection(username=username, url=url)
 
 
 def test_tables():
     """ Test tables. """
-    conn = Connection(username=username, url=url)
     results = conn.tables
     assert isinstance(results, list)
 
 
 def test_search_tables():
-    conn = Connection(username=username, url=url)
     results = conn.search('term')
     assert isinstance(results, list)
 
 
 def test_get_table_with_bad_ids():
-    conn = Connection(username=username, url=url)
     # with pytest.raises(TableNotFoundError):
     result = conn.get_table(10000000000)
-    assert isinstance(result, type(None))  # hacky
+    assert isinstance(result, type(None))  # hack
     result = conn.get_table('test string')
     assert isinstance(result, type(None))
 
 
 def test_create_empty_table():
-    conn = Connection(username=username, url=url)
     t1 = conn.create_table(name='test', description='test empty')
     t2 = conn.get_table(t1.id)
     assert t1 == t2
 
 
 def test_create_table_with_duplicate_attributes():
-    conn = Connection(username=username, url=url)
     t1 = conn.create_table(name='test', description='test empty')
     t2 = conn.create_table(name='test', description='test empty')
     assert t2.id == t1.id + 1
@@ -53,21 +49,30 @@ def test_create_table_from_file():
     with open(test_file, 'w') as f:
         f.write(data)
     test_description = 'test file ' + test_name
-    conn = Connection(username=username, url=url)
     t1 = conn.create_table(name=test_name, description=test_description,
                            inputfile=test_file)
     assert ['col1', 'col2'] == [col.name for col in t1.columns]
     fields = [col.field for col in t1.columns]
     rows = [row for row in t1]
-    # expected_row1 = {fields[0]: 1, fields[1]: 'a', u'qgrid': 1}
-    # expected_row2 = {fields[0]: 2, fields[1]: 'b', u'qgrid': 2}
-    # assert rows == [expected_row1, expected_row2]  # this fails
     assert rows[0][fields[0]] == 1
-    # assert isinstance(rows[0][fields[0]], int)  # this fails, it's 1.0 not 1
     assert rows[0][fields[1]] == 'a'
     assert rows[1][fields[0]] == 2
-    # assert isinstance(rows[1][fields[0]], int)  # this fails, it's 2.0 not 2
     assert rows[1][fields[1]] == 'b'
     t2 = conn.get_table(t1.id)
     assert t1 == t2
     os.remove(test_file)
+
+
+def test_create_table_from_df():
+    cols = ['col1', 'col2']
+    data = {1: 'a', 2: 'b'}
+    df = pd.DataFrame(list(data.iteritems()), columns=cols)
+    t1 = conn.save_df(df, name='testDataFrame', description="test")
+    t2 = conn.get_table(t1.id)
+    assert ['col1', 'col2'] == [col.name for col in t2.columns]
+    fields = [col.field for col in t2.columns]
+    rows = [row for row in t2]
+    assert rows[0][fields[0]] == 1
+    assert rows[0][fields[1]] == 'a'
+    assert rows[1][fields[0]] == 2
+    assert rows[1][fields[1]] == 'b'
